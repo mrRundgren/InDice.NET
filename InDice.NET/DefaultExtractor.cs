2c53f83ad2054ae93ee217497f0b432df51a3904
anders.rundgren <anders.rundgren@elicit.se>
2022-11-23 08:35:04 +0100

    Fixed some more formatting

--- InDice.NET/DefaultExtractor.cs
+++ InDice.NET/DefaultExtractor.cs
@@ -1,41 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace InDice.NET;
 
-namespace InDice.NET
+public class DefaultExtractor : IExtractor
 {
-    public class DefaultExtractor : IExtractor
+    public IEncoder Encoder { get; set; } = null!;
+
+    public DefaultExtractor()
     {
-        public IEncoder Encoder { get; set; } = null!;
+        Encoder = new DefaultEncoder();
+    }
 
-        public DefaultExtractor()
-        {
-            Encoder = new DefaultEncoder();
-        }
+    public DefaultExtractor(string unsafeChars)
+    {
+        Encoder = new DefaultEncoder(unsafeChars);
+    }
 
-        public DefaultExtractor(string unsafeChars)
-        {
-            Encoder = new DefaultEncoder(unsafeChars);
-        }
+    public IEnumerable<string> ExtractExplicitMatches(string source, out string newSource) =>
+        source.ExtractSubStrings(('"', '"'), Encoder, out newSource).Union(newSource.ExtractSubStrings(('+', ' '), Encoder, out newSource));
 
-        public IEnumerable<string> ExtractExplicitMatches(string source, out string newSource) =>
-            source.ExtractSubStrings(('"', '"'), Encoder, out newSource).Union(newSource.ExtractSubStrings(('+', ' '), Encoder, out newSource));
+    public IEnumerable<string> ExtractExcludedMatches(string source, out string newSource) =>
+        source.ExtractSubStrings(('-', ' '), Encoder, out newSource);
 
-        public IEnumerable<string> ExtractExcludedMatches(string source, out string newSource) =>
-            source.ExtractSubStrings(('-', ' '), Encoder, out newSource);
+    public IEnumerable<string> ExtractImplicitMatches(string source, out string newSource)
+    {
+        string backup = source;
 
-        public IEnumerable<string> ExtractImplicitMatches(string source, out string newSource)
-        {
-            string backup = source;
+        ExtractExplicitMatches(source, out source);
+        ExtractExcludedMatches(source, out source);
 
-            ExtractExplicitMatches(source, out source);
-            ExtractExcludedMatches(source, out source);
-
-            newSource = backup.Difference(source);
-            
-            return source.Split(' ').Select(_ => Encoder.Encode(_)).Where(_ => !string.IsNullOrWhiteSpace(_));
-        }
+        newSource = backup.Difference(source);
+        
+        return source.Split(' ').Select(_ => Encoder.Encode(_)).Where(_ => !string.IsNullOrWhiteSpace(_));
     }
 }
